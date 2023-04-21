39bbe5136c0702ee42c70c763b9bdf2c34052bde
Xin Le Chong <s10221973@connect.np.edu.sg>
2023-04-21 16:58:06 +0800

    2

--- Question2.java
+++ Question2.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Question2
 {
   public static void main(String[] args)
-  {
-    /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
+  {  
     Scanner in = new Scanner(System.in);
+
+    System.out.print("Enter your height (in meters): ");
     double height = in.nextDouble();
+
+    System.out.print("Enter your weight (in kilograms): ");
+    double weight = in.nextDouble();
+
+    double bmi = weight / (height * height);
+    System.out.printf("Your BMI is: %.2f", bmi);
   }
 }
