2e900e09caac20dbefe46040dbd838744e25cbaf
Xin Le Chong <s10221973@connect.np.edu.sg>
2023-04-21 17:13:04 +0800

    3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,11 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+
+    System.out.print("Enter an integer: ");
+    int num = in.nextInt();
+
+    int result = num * num;
+    System.out.printf("%d multiplied by itself is: %d", num, result);
   }
 }
