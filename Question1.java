47163af4881f81c59fa8506fb84b0672e8afbb93
Xin Le Chong <s10221973@connect.np.edu.sg>
2023-04-21 21:22:43 +0800

    trying i think

--- Question1.java
+++ Question1.java
@@ -2,8 +2,11 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    
-     System.out.println("Hello World!");
-     
+    /**
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
+    System.out.println("Hello World!");
   }
 }
